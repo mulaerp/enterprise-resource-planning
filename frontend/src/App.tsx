aa71678b3248425d0b035a2f7ee7c98b0d9faa21
cc8t <cc8t@users.noreply.github.com>
2025-11-19 22:59:29 +0800

    ff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *     https://github.com/cc8t/mula-erp/blob/main/LICENSE
+ *     https://github.com/mulaerp/enterprise-resource-planning/blob/main/LICENSE
  * 
  * Change Date: 2029-01-19
  * Change License: GNU General Public License v3.0 or later
